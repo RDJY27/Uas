fdf121ed16ed70762e0c76caaaa21927fef5a7dd
ostnyonge <hamdii340@gmail.com>
2023-06-18 02:59:15 +0700

    fixing nav-link gabisa di klik

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -28,15 +28,15 @@ class User extends CI_Controller
     public function edit()
     {
         $data['title'] = 'Edit Profile';
- update_uas6
-        $data['user'] = $this->db->get_where('user', ['email' =>
-        $this->session->userdata('email')])->row_array();
+        $data['user'] = $this->db->get_where('user', [
+            'email' =>
+            $this->session->userdata('email')
+        ])->row_array();
 
         $data['user'] = $this->db->get_where('user', [
             'email' =>
             $this->session->userdata('email')
         ])->row_array();
- master
 
         $this->form_validation->set_rules('name', 'Full Name', 'required|trim');
 
@@ -53,43 +53,40 @@ class User extends CI_Controller
             $email = $this->input->post('email');
 
             //cek jika ada gambar yang di update
- update_uas6
             $upload_image = $_FILES['image']['name'];
             if ($upload_image) {
                 $config['allowed_types'] = 'gif|jpg|png|jpeg';
 
-            $uploud_image = $_FILES['image']['name'];
-            if ($uploud_image) {
-                $config['allowed_types'] = 'gif|jpg|png';
- master
-                $config['max_size'] = '2048';
-                $config['upload_path'] = './assets/img/profile/';
-                $this->load->library('upload', $config);
+                $uploud_image = $_FILES['image']['name'];
+                if ($uploud_image) {
+                    $config['allowed_types'] = 'gif|jpg|png';
+                    $config['max_size'] = '2048';
+                    $config['upload_path'] = './assets/img/profile/';
+                    $this->load->library('upload', $config);
 
-                if ($this->upload->do_upload('image')) {
- update_uas6
-                    $old_image = $data['user']['image'];
-                    if ($old_image != 'default.jpg') {
-                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    if ($this->upload->do_upload('image')) {
+                        $old_image = $data['user']['image'];
+                        if ($old_image != 'default.jpg') {
+                            unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                        }
+
+                        $new_image = $this->upload->data('file_name');
+                        $this->db->set('image', $new_image);
+                    } else {
+                        echo $this->upload->display_errors();
                     }
-
- master
-                    $new_image = $this->upload->data('file_name');
-                    $this->db->set('image', $new_image);
-                } else {
-                    echo $this->upload->display_errors();
                 }
-            }
 
-            $this->db->set('name', $name);
-            $this->db->where('email', $email);
+                $this->db->set('name', $name);
+                $this->db->where('email', $email);
 
-            $this->db->update('user');
+                $this->db->update('user');
 
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
             Congratulations, your Profile has been Upadated!
-          </div>');
-            redirect('user');
+            </div>');
+                redirect('user');
+            }
         }
     }
 }
--- application/views/index/index.php
+++ application/views/index/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Yummy Bootstrap Template - Index</title>
+    <title>Seduh.Tisane</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
     <link rel="icon" type="image" href="assets/img/favicon.ico">
